26032d5a3ac81e0f73858899c227f9b128eb46d3
Alexander <renyreny125tt@gmail.com>
2022-10-30 16:39:02 +0300

    feat: add constants

---
+++ src/constants/queryVariables.js
@@ -0,0 +1,10 @@
+const URL_SERVER = 'https://api.apilayer.com/exchangerates_data/convert';
+const API_KEY = 'sCCPc1OpI0R7TDJwt4SRdWKxerQ77qqV';
+const OK = 200;
+const CREATED = 201;
+const NoContent = 204;
+const UNAUTHORIZED = 401;
+const FORBIDDEN = 403;
+const CONFLICT = 409;
+
+export { URL_SERVER, API_KEY, OK, CREATED, FORBIDDEN, CONFLICT, UNAUTHORIZED, NoContent };
