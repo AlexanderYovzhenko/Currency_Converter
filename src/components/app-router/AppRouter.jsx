8142e67545e5fa322c14eaff51db8fef305456e8
Alexander <renyreny125tt@gmail.com>
2022-10-30 15:29:44 +0300

    refactor: move files

--- src/components/app-router/AppRouter.jsx
+++ src/components/app-router/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import ConverterPage from '../../pages/ConverterPage';
+import ConverterPage from '../../pages/converter-page/ConverterPage';
 
 const AppRouter = () => {
   return (
