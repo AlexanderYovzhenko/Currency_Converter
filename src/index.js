8c78f1c9e67a4bf79f3cce6b935429bc3c9b612b
Alexander <renyreny125tt@gmail.com>
2023-06-09 16:02:20 +0300

    refactor: delete react-scripts

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <Provider store={ store }>
-        <App />
-      </Provider> 
-    </HashRouter>   
-  </React.StrictMode>
+  <HashRouter>
+    <Provider store={ store }>
+      <App />
+    </Provider> 
+  </HashRouter>   
 );
